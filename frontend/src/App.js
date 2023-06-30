455bfc5923cd9c5b6fe71854163d64a06e9cb330
ParJavadian <parjavadian@gmail.com>
2023-07-01 00:10:59 +0330

    material tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,9 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import MyNavbar from "./components/MyNavBar";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  return (
-    <div className="App">
-      <button
-        onClick={() => {
-          setCounter((prevCount) => prevCount + 1);
-        }}
-      >
-        +
-      </button>
-      <h1>{counter}</h1>
-      <button
-        onClick={() => {
-          setCounter((prevCount) => prevCount - 1);
-        }}
-      >
-        -
-      </button>
-    </div>
-  );
+  return <MyNavbar />;
 };
 
 export default App;
