a7dd5628ee58e101c27dbad8fe2a44f8c5d3a091
ParJavadian <parjavadian@gmail.com>
2023-07-01 14:42:53 +0330

    improve search component

--- frontend/src/components/SearchInput.jsx
+++ frontend/src/components/SearchInput.jsx
@@ -1,9 +1,26 @@
 import { Button, Input } from "@material-tailwind/react";
-import SearchIcon from "../images/search.png";
+import * as Unicons from "@iconscout/react-unicons";
+function handleKeyDown(e) {
+  if (e.key === "Enter") {
+    //TODO : implement do search
+    doSearch();
+    console.log("do validate");
+  }
+}
+//TODO
+function doSearch() {}
 export default function SearchInput() {
   return (
     <div className="relative flex w-full gap-2 md:w-max">
       <Input
+        onKeyDown={handleKeyDown}
+        variant="oulined"
+        // placeholder="Search for jobs"
+        label="Search for jobs"
+        type="search"
+        icon={<Unicons.UilSearch onClick={doSearch} />}
+      />
+      {/* <Input
         type="search"
         label="Search for jobs"
         className="pr-20"
@@ -13,7 +30,7 @@ export default function SearchInput() {
       />
       <Button size="sm" className="!absolute right-1 top-1 rounded">
         Search
-      </Button>
+      </Button> */}
     </div>
   );
 }
