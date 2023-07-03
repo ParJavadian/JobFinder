b994cca4e2517d551d16427115d55ae0f1954df1
your_username <royadaneshi2001@gmail.com>
2023-07-03 21:26:20 +0330

    get search values

--- frontend/src/components/SearchInput.jsx
+++ frontend/src/components/SearchInput.jsx
@@ -1,57 +1,57 @@
+import React, { useState } from "react";
 import { IconButton, Input } from "@material-tailwind/react";
 import * as Unicons from "@iconscout/react-unicons";
-function handleKeyDown(e) {
-  if (e.key === "Enter") {
-    //TODO : implement do search
-    doSearch();
-    console.log("do validate");
-  }
-}
-//TODO
-function doSearch() {}
+
 export default function SearchInput() {
+  const [jobTitle, setJobTitle] = useState("");
+  const [location, setLocation] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      doSearch();
+      console.log("do validate");
+    }
+  };
+
+  const doSearch = () => {
+//search...
+//values are in jobTitle and location and category
+    console.log("Search:", jobTitle, location, category);
+  };
+
   return (
-    <>
-      <div className="flex items-center justify-center">
-        <div className="flex bg-blue-50	flex-row gap-2 w-5/6 rounded-lg border-blue-300 border p-4">
-          <Input
-            variant="oulined"
-            // placeholder="Search for jobs"
-            label="Job Title"
-            type="search"
-            className="bg-white"
-          />
-          <Input
-            variant="oulined"
-            // placeholder="Search for jobs"
-            label="Location"
-            type="search"
-            className="bg-white"
-          />
-          <Input
-            onKeyDown={handleKeyDown}
-            variant="oulined"
-            // placeholder="Search for jobs"
-            label="Category"
-            type="search"
-            className="bg-white"
-          />
-          <IconButton onClick={doSearch} className="w-24 h-24 flex-none">
-            <Unicons.UilSearch />{" "}
-          </IconButton>
-          {/* <Input
-        type="search"
-        label="Search for jobs"
-        className="pr-20"
-        containerProps={{
-          className: "min-w-[288px]",
-        }}
-      />
-      <Button size="sm" className="!absolute right-1 top-1 rounded">
-        Search
-      </Button> */}
-        </div>
+    <div className="flex items-center justify-center">
+      <div className="flex bg-blue-50	flex-row gap-2 w-5/6 rounded-lg border-blue-300 border p-4">
+        <Input
+          variant="outlined"
+          label="Job Title"
+          type="search"
+          className="bg-white"
+          value={jobTitle}
+          onChange={(e) => setJobTitle(e.target.value)}
+        />
+        <Input
+          variant="outlined"
+          label="Location"
+          type="search"
+          className="bg-white"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <Input
+          onKeyDown={handleKeyDown}
+          variant="outlined"
+          label="Category"
+          type="search"
+          className="bg-white"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
+        <IconButton onClick={doSearch} className="w-24 h-24 flex-none">
+          <Unicons.UilSearch />
+        </IconButton>
       </div>
-    </>
+    </div>
   );
 }
