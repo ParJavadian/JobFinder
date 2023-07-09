2a33d5586540debb43e3096524d42f7ce00407f5
your_username <royadaneshi2001@gmail.com>
2023-07-09 19:21:47 +0330

    add token when login and send request to back and show error message

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -1,3 +1,4 @@
+import React, { useState ,useEffect} from "react";
 import {
   Card,
   CardHeader,
@@ -5,26 +6,55 @@ import {
   CardFooter,
   Typography,
   Input,
-  Checkbox,
   Button,
 } from "@material-tailwind/react";
-import SignUp from "../pages/SignUp";
-import SignIn from "../pages/Login";
-import { useNavigate } from "react-router-dom";
-import React, { useState } from 'react';
-
-import PropTypes from 'prop-types';
 
 export default function LoginCard() {
-  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  useEffect(() => {
+    let timer;
+    if (error) {
+      // Set a timeout to close the alert after 3 seconds
+      timer = setTimeout(() => {
+        closeAlert();
+      }, 5000);
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [error]);
 
-  const handleSignIn = () => {
-    navigate("/Login");
+  const handleSignIn = async () => {
+    try {
+      // Send login request to the backend
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
 
+      if (response.ok) {
+        // Login successful, retrieve the token from the response
+        const { token } = await response.json();
+        localStorage.setItem("token", token);
+        console.log("token:", token);
+      } else {
+        // const errorResponse = await response.text();
+        setError("Please enter valid Email and Password");
+      }
+    } catch (error) {
+      console.log("An error occurred", error);
+    }
   };
 
+  const closeAlert = () => {
+    setError("");
+  };
 
- 
   return (
     <Card className="w-96">
       <CardHeader
@@ -32,20 +62,27 @@ export default function LoginCard() {
         color="blue"
         className="mb-4 grid h-28 place-items-center"
       >
-        <Typography variant="h3" color="white"  >
+        <Typography variant="h3" color="white">
           Sign In
         </Typography>
       </CardHeader>
       <CardBody className="flex flex-col gap-4">
-        <Input label="Email" size="lg" onChange={e => setEmaill(e.target.value)}/>
-        <Input label="Password" size="lg" onChange={e => setPassword(e.target.value)}/>
-        {/* <div className="-ml-2.5">
-          <Checkbox label="Remember Me" />
-        </div> */}
+        <Input
+          label="Email"
+          size="lg"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          label="Password"
+          size="lg"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </CardBody>
       <CardFooter className="pt-0">
-        
-        <Button variant="gradient" fullWidth   onClick={handleSignIn}>
+        <Button variant="gradient" fullWidth onClick={handleSignIn}>
           Sign In
         </Button>
         <Typography variant="small" className="mt-6 flex justify-center">
@@ -61,6 +98,18 @@ export default function LoginCard() {
           </Typography>
         </Typography>
       </CardFooter>
+      {error && (
+  <div className="fixed top-16 right-4 bg-red-300 text-white px-4 py-2 rounded">
+    <span>{error}</span>
+    <button
+      className="ml-2 text-white font-bold"
+      onClick={closeAlert}
+    >
+      X
+    </button>
+  </div>
+)}
+
     </Card>
   );
 }
