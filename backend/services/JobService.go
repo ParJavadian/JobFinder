0f67db6bdb34935500b96ee7da53e7b1082cb7bc
kiana-msz <kianamousazadeh2018@gmail.com>
2023-07-09 23:56:45 +0800

    JobService

--- backend/services/JobService.go
+++ backend/services/JobService.go
@@ -1,4 +1,35 @@
 package services
 
+import (
+	"JobFinder/backend/persistence"
+	"errors"
+	"time"
+)
+
 type JobService struct {
+    jobRepository persistence.JobRepository
 }
+
+func NewJobService(jobRepository persistence.JobRepository) *JobService {
+	return &JobService{
+		jobRepository: jobRepository,
+	}
+}
+
+func (s *JobService) CreateJob(job *persistence.Job) error {
+	// Check if the ID is already registered
+	existingJob, _ := s.jobRepository.GetJobByID(job.ID)
+	if existingJob != nil {
+		return errors.New("there is already a job with this ID")
+	}
+
+	// Save the job to the database
+	err = s.jobRepository.CreateJob(job)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+
+
