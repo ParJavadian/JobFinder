3949f83cb6c11d42cd1ce23cd14316a5acec9c17
your_username <royadaneshi2001@gmail.com>
2023-07-10 09:38:30 +0330

    send data

--- frontend/src/routes/Allroutes.jsx
+++ frontend/src/routes/Allroutes.jsx
@@ -12,10 +12,10 @@ export default function AllRoutes() {
   return (
     <Router history={history}>
       <Routes>
-        {/* <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/company-dashboard/*" element={<CompanyDashbord />} />
-        <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
         <Route path="/seeker-dashboard/*" element={<SeekerDashboard />} />
         <Route path="/details/*" element={<JobDetails />} />
       </Routes>
